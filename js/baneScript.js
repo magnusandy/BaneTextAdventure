9637a0c6104de50861f33fdb58b82f763c61f22b
magnusandy <magnusandy@gmail.com>
2015-04-09 03:38:03 -0600

    cool, got xml working
    now reading commands from and xml/json file

--- js/baneScript.js
+++ js/baneScript.js
@@ -1,5 +1,5 @@
 var currentPlayer = "none";
-var currentPlace = "0";
+var currentPlace = 0;
 var data = '{"name": "none", "place": "0"}';
 var story = null;
 
@@ -17,7 +17,9 @@ function inputEnter(e, input)
 	if(e.keyCode == 13)
 	{
 		postOutput(input);
-		alert(story.getElementsByTagName("story")[0].getElementsByTagName("1").nodeValue);
+		//alert(story.getElementsByTagName("story")[0].getElementsByTagName("none")[1].getElementsByTagName("commands")[0].childNodes[0].nodeValue);
+		var commandOutput = command(currentPlayer, currentPlace, input)
+		postOutput(commandOutput);
 		document.getElementById("consoleInput").value = "";
 	}
 }
@@ -31,4 +33,28 @@ function postOutput(input)
 		console.innerHTML = console.innerHTML + " " + input + "\n>"
 		
 		console.scrollTop = console.scrollHeight;
+}
+
+function command(character, place, command)
+{
+	var commands = story.getElementsByTagName("story")[0].getElementsByTagName(character)[place].getElementsByTagName("commands")[0].childNodes[0].nodeValue
+	commands = JSON.parse(commands);
+	for(var i = 0; i<commands.length; i++)
+	{
+		//check if the command is listed
+		if(commands[i].command == command)
+		{
+			//see if command is critical
+			if(commands[i].crit == "true")
+			{
+				currentPlace = currentPlace+1;
+			}
+			return commands[i].output;
+		}
+	}
+	return "command not found";
+	//alert(commands[0].command)
+	//command is recognized, dont move on
+	//command is recongnized, move on (increment place)
+	//command is not recongnized, dont move on
 }
