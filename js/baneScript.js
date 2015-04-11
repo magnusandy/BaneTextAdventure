704ebdf1f26b2bfa668e6062d770b80419ed755b
magnusandy <magnusandy@gmail.com>
2015-04-10 20:20:31 -0600

    no more cia for now

--- js/baneScript.js
+++ js/baneScript.js
@@ -72,11 +72,11 @@ function command(character, place, command)
 			return "You are driving a jeep, in the distance you see a plane \n> What would you like to do?";
 		}
 		//start story as cia now with game of thrones references
-		if((command == "cia") || (command == "cia man") || (command == "littlefinger") || (command == "little finger") || (command == "petyr baelish"))
+		/*if((command == "cia") || (command == "cia man") || (command == "littlefinger") || (command == "little finger") || (command == "petyr baelish"))
 		{
 			currentPlayer = "cia";
 			return "You are standing outside, in front of a commuter plane.\n> there are Special Ops soliders with you \n> in the distance you see a jeep driving your way. \n> what would you like to do?"
-		}
+		}*/
 	}
 	//reads in the current part of the story based on your character and place, and searches through for the command you specified
 	var commands = story.getElementsByTagName("story")[0].getElementsByTagName(character)[place].getElementsByTagName("commands")[0].childNodes[0].nodeValue;
